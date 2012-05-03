a15f0040160d6170cf4a11fe9ccf9d8cd0b338ab
Lun Jiang <lun.jiang@gmail.com>
2012-05-03 20:45:51 +0200

    test avec 3 threads

--- src/test_ordo.c
+++ src/test_ordo.c
@@ -18,25 +18,29 @@ static void * threadfunc(void * arg)
 
 int main(int argc, char *argv[])
 {
-  thread_t thread1, thread2;
-  void *retval1, *retval2;
-  int err;
-
-  printf("le main lance 2 threads...\n");
-  err = thread_create_with_prio(&thread1, threadfunc, "thread1",0);
-  assert(!err);
-  err = thread_create_with_prio(&thread2, threadfunc, "thread2",-5);
-  assert(!err);
-  printf("le main a lancé les threads %p et %p\n",
-	 thread1, thread2);
-
-  printf("le main attend les threads\n");
-  err = thread_join(thread2, &retval2);
-  assert(!err);
-  err = thread_join(thread1, &retval1);
-  assert(!err);
-  printf("les threads ont terminé en renvoyant '%s' et '%s'\n",
-	 (char *) retval1, (char *) retval2);
+    thread_t thread1, thread2, thread3;
+    void *retval1, *retval2, *retval3;
+    int err;
+    
+    printf("le main lance 3 threads...\n");
+    err = thread_create_with_prio(&thread1, threadfunc, "thread1", -5);
+    assert(!err);
+    err = thread_create_with_prio(&thread2, threadfunc, "thread2", 0);
+    assert(!err);
+    err = thread_create_with_prio(&thread3, threadfunc, "thread3", 5);
+    assert(!err);
+    printf("le main a lancé les threads %p, %p et %p\n",
+	   thread1, thread2, thread3);
+    
+    printf("le main attend les threads\n");
+    err = thread_join(thread3, &retval3);
+    assert(!err);
+    err = thread_join(thread2, &retval2);
+    assert(!err);
+    err = thread_join(thread1, &retval1);
+    assert(!err);
+    printf("les threads ont terminé en renvoyant '%s', '%s' et '%s'\n",
+	   (char *) retval1, (char *) retval2, (char *) retval3);
 
   return 0;
 }
